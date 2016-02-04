91e3bbc7714442251c4c501742d7e2e61478fab2
Jeffrey LeCompte <jeffreylec@gmail.com>
2016-02-03 23:30:05 -0800

    this branch is for hitbox reworking. i will work on this more in the morning when my brain feels better. I have a good idea what I want to do and will work through it on paper and debug. /j

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,7 @@ function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDu
  * tru
  */
 function distance(a, b) {
-   // console.log("Received parameters: (" + a.x + "," + a.y + ") , (" + b.x + "," + b.y + ")");
+    // console.log("Received parameters: (" + a.x + "," + a.y + ") , (" + b.x + "," + b.y + ")");
 
     var dx = Math.abs(a.x - b.x);
     var dy = Math.abs(a.y - b.y);
@@ -72,11 +72,11 @@ Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
     ctx.translate(-(locX + (this.frameWidth * scaleBy) / 2)  , -(locY + (this.frameHeight * scaleBy) / 2));
 
     ctx.drawImage(this.spriteSheet,
-                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
-                  this.frameWidth, this.frameHeight,
-                  locX, locY,
-                  this.frameWidth * scaleBy,
-                  this.frameHeight * scaleBy);
+        index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+        this.frameWidth, this.frameHeight,
+        locX, locY,
+        this.frameWidth * scaleBy,
+        this.frameHeight * scaleBy);
 
     ctx.restore();
 
@@ -108,6 +108,109 @@ Background.prototype.draw = function (ctx) {
     //ctx.fillRect(0,500,800,300);
     ctx.drawImage(this.bg, 0, 0);
     Entity.prototype.draw.call(this);
+};
+
+function Hitbox(game) {
+    this.radius = 0;
+    this.name = "Hitbox";
+}
+
+Hitbox.prototype = new Entity();
+Hitbox.prototype.constructor = Hitbox;
+
+Hitbox.prototype.collide = function (other) {
+    return distance(this, other) < this.radius + other.radius;
+};
+
+Hitbox.prototype.collideLeft = function () {
+    return (this.x - this.radius) < 0;
+};
+
+Hitbox.prototype.collideRight = function () {
+    return (this.x + this.radius) > 800;
+};
+
+Hitbox.prototype.collideTop = function () {
+    return (this.y - this.radius) < 0;
+};
+
+Hitbox.prototype.collideBottom = function () {
+    return (this.y + this.radius) > 800;
+};
+
+Hitbox.prototype.update = function () {
+    Entity.prototype.update.call(this);
+
+    // TODO: edit to work with zombie touching player
+    if (this.collideLeft() || this.collideRight()) {
+        if (this.collideLeft()) this.x = this.radius;
+        if (this.collideRight()) this.x = 800 - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+    // TODO: edit to work with zombie touching player
+    if (this.collideTop() || this.collideBottom()) {
+        if (this.collideTop()) this.y = this.radius;
+        if (this.collideBottom()) this.y = 800 - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+
+    for (var i = 0; i < this.game.entities.length; i++) {
+        var ent = this.game.entities[i];
+        if (ent !== this && this.collide(ent)) {
+            var temp = { x: this.velocity.x, y: this.velocity.y };
+
+            var dist = distance(this, ent);
+            var delta = this.radius + ent.radius - dist;
+            var difX = (this.x - ent.x)/dist;
+            var difY = (this.y - ent.y)/dist;
+
+            this.x += difX * delta / 2;
+            this.y += difY * delta / 2;
+            ent.x -= difX * delta / 2;
+            ent.y -= difY * delta / 2;
+
+            this.velocity.x = ent.velocity.x * friction;
+            this.velocity.y = ent.velocity.y * friction;
+            ent.velocity.x = temp.x * friction;
+            ent.velocity.y = temp.y * friction;
+            this.x += this.velocity.x * this.game.clockTick;
+            this.y += this.velocity.y * this.game.clockTick;
+            ent.x += ent.velocity.x * this.game.clockTick;
+            ent.y += ent.velocity.y * this.game.clockTick;
+        }
+
+        if (ent != this && this.collide({ x: ent.x, y: ent.y, radius: this.visualRadius })) {
+            var dist = distance(this, ent);
+            if (this.it && dist > this.radius + ent.radius + 10) {
+                var difX = (ent.x - this.x)/dist;
+                var difY = (ent.y - this.y)/dist;
+                this.velocity.x += difX * acceleration / (dist*dist);
+                this.velocity.y += difY * acceleration / (dist * dist);
+                var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
+                if (speed > maxSpeed) {
+                    var ratio = maxSpeed / speed;
+                    this.velocity.x *= ratio;
+                    this.velocity.y *= ratio;
+                }
+            }
+            if (ent.it && dist > this.radius + ent.radius) {
+                var difX = (ent.x - this.x) / dist;
+                var difY = (ent.y - this.y) / dist;
+                this.velocity.x -= difX * acceleration / (dist * dist);
+                this.velocity.y -= difY * acceleration / (dist * dist);
+                var speed = Math.sqrt(this.velocity.x * this.velocity.x + this.velocity.y * this.velocity.y);
+                if (speed > maxSpeed) {
+                    var ratio = maxSpeed / speed;
+                    this.velocity.x *= ratio;
+                    this.velocity.y *= ratio;
+                }
+            }
+        }
+    }
 };
 
 function Zombie(game) {
@@ -278,9 +381,9 @@ Player.prototype.shoot = function(endX, endY) {
 };
 
 /**
-   * Moves the player.
-   * @param xTrans distance to move left or right
-   * @param yTrans distance to move up or down
+ * Moves the player.
+ * @param xTrans distance to move left or right
+ * @param yTrans distance to move up or down
  */
 Player.prototype.move = function(xTrans, yTrans) {
     this.x += xTrans;
@@ -545,16 +648,16 @@ var Key = {
         this._pressed[event.keyCode] = true;
     },
     onKeyUp: function(event) {
-       this._pressed[event.keyCode] = false;
+        this._pressed[event.keyCode] = false;
         // delete this._pressed[event.keyCode];
-       // var index = this._pressed.indexOf(event.keyCode);
-       // this._pressed.splice(index, 1);
+        // var index = this._pressed.indexOf(event.keyCode);
+        // this._pressed.splice(index, 1);
     },
     keyPressed: function() {
         /*for (var i = 0; i < this._pressed.length; i++) {
-            if (this._pressed[i]) return true;
-        }
-        return false;*/
+         if (this._pressed[i]) return true;
+         }
+         return false;*/
         return this._pressed.length === 0;
 
     }
