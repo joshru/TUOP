45a91a2d245035687fd2f1d124127fb9deeda1d6
Brandon <brandb94@uw.edu>
2016-02-26 17:01:41 -0800

    Added closing brackets to functions that were missing them in Player

--- scripts/player.js
+++ scripts/player.js
@@ -60,7 +60,7 @@ Player.prototype.constructor = Player;
 /**
  * creates a bullet and adds it to the game's bullet data structure
  */
-Player.prototype.shoot = function (endX, endY) {
+Player.prototype.shoot = function (endX, endY, firingMode) {
     var bulletX = this.x + (this.animations.idle.frameWidth * this.scale) / 2;
     var bulletY = this.y + (this.animations.idle.frameWidth * this.scale) / 2;
 
@@ -172,14 +172,13 @@ Player.prototype.handleMovementInput = function () {
 Player.prototype.update = function () {
     this.convertToOffScreen();
     this.handleMovementInput();
-    this.hitbox.updateXY(this.x + (this.animations.idle.frameWidth * this.scale)  / 2,
-                         this.y + (this.animations.idle.frameHeight * this.scale) / 2);
+    this.hitbox.updateXY(this.x + (this.animations.idle.frameWidth * this.scale) / 2,
+        this.y + (this.animations.idle.frameHeight * this.scale) / 2);
 
     //console.log("player x: " + this.x + " | player y: " + this.y);
     if (this.states.CURRENT_GUN != 'sniper') this.drawLazer = false;
 
     if (!Key.keyPressed()) this.state = this.states.IDLE;
-
 
 
     //if (this.game.RELOAD) {
@@ -206,41 +205,40 @@ Player.prototype.update = function () {
     if (this.game.leftClick) {
         if (globals.debug) console.log("shooting");
 
-    //SHOOT DA GUNZ
-    if(this.game.firing) {
+        //SHOOT DA GUNZ
+        if (this.game.firing) {
 
-        var currentTime = Date.now();
+            var currentTime = Date.now();
 
-        if ((currentTime - this.lastShotFired) / 1000 > this.weaponShotDelay) {
-            this.shoot(globals.mousePosition.x, globals.mousePosition.y, this.currentFiringMode);
-            this.lastShotFired = Date.now();
+            if ((currentTime - this.lastShotFired) / 1000 > this.weaponShotDelay) {
+                this.shoot(globals.mousePosition.x, globals.mousePosition.y, this.currentFiringMode);
+                this.lastShotFired = Date.now();
+            }
+
         }
 
-    }
 
-
-        if(this.game.mouseup) {
-           // mouseStillDown = false;
+        if (this.game.mouseup) {
+            // mouseStillDown = false;
             this.state = this.states.IDLE;
         }
 
 
+        if (this.animations.reloadPistol.isDone()) {
+            this.game.RELOAD = false;
+            this.animations.reloadPistol.elapsedTime = 0;
+            this.game.leftClick = false;
+        } else if (!this.states.MOVING && !this.states.SHOOTING) {
+            this.state = this.states.IDLE;
 
-    if (this.animations.reloadPistol.isDone()) {
-        this.game.RELOAD = false;
-        this.animations.reloadPistol.elapsedTime = 0;
-        this.game.leftClick = false;
-    } else if (!this.states.MOVING && !this.states.SHOOTING) {
-        this.state = this.states.IDLE;
+        }
 
+
+        Entity.prototype.update.call(this);
     }
-
-
-
-    Entity.prototype.update.call(this);
 };
 
-Player.prototype.checkForWeaponSwap = function() {
+Player.prototype.checkForWeaponSwap = function () {
     if (Key.isDown(Key.ONE)) {
         this.states.CURRENT_GUN = 'pistol';
         console.log("pistol equipped");
@@ -248,7 +246,7 @@ Player.prototype.checkForWeaponSwap = function() {
         this.currentFiringMode = "full auto";
     }
 
-    if (Key.isDown(Key.TWO)){
+    if (Key.isDown(Key.TWO)) {
         this.states.CURRENT_GUN = "assault rifle";
         console.log("assault rifle equipped");
         this.weaponShotDelay = .1;
@@ -275,14 +273,13 @@ Player.prototype.checkForWeaponSwap = function() {
     }
 
 
-
 };
 
-Player.prototype.throwGrenade = function() {
+Player.prototype.throwGrenade = function () {
     var startX = this.x + (this.animations.idle.frameWidth * this.scale);
     var startY = this.y + (this.animations.idle.frameWidth * this.scale) / 2;
 
-    this.game.addEntity(new Grenade(startX, startY, globals.mousePosition.x, globals.mousePosition.y, this.game ));
+    this.game.addEntity(new Grenade(startX, startY, globals.mousePosition.x, globals.mousePosition.y, this.game));
 };
 
 /**
@@ -305,86 +302,86 @@ Player.prototype.draw = function (ctx) {
         //this.animations.idle.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
         currAnim = this.animations.idle;
 
-    if (this.state === this.states.RELOADING) {
-        this.animations.reloadPistol.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
-    }
-    if (this.state === this.states.SHOOTING) {
-        this.animations.idleFeet.drawFrame(this.game.clockTick, ctx, this.x + 12, this.y + 17, this.scale);
-        //this.animations.shootPistol.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
-        currAnim = this.animations.shootPistol;
+        if (this.state === this.states.RELOADING) {
+            this.animations.reloadPistol.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
+        }
+        if (this.state === this.states.SHOOTING) {
+            this.animations.idleFeet.drawFrame(this.game.clockTick, ctx, this.x + 12, this.y + 17, this.scale);
+            //this.animations.shootPistol.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
+            currAnim = this.animations.shootPistol;
 
-    }
-    currAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
+        }
+        currAnim.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
 
-    if (globals.debug) {
-        ctx.font = "12px Courier New";
-        ctx.fillText("x: " + Math.round(this.x) + " y: " + Math.round(this.y), this.x, this.y + 10);
-        ctx.fillText("sX: " + Math.round(this.screenX) + " | sY: " + Math.round(this.screenY), this.x, this.y + 20);
-        ctx.fillText("wX: " + Math.round(this.worldX) + "  | wY: " + Math.round(this.worldX), this.x, this.y + 30);
-    }
+        if (globals.debug) {
+            ctx.font = "12px Courier New";
+            ctx.fillText("x: " + Math.round(this.x) + " y: " + Math.round(this.y), this.x, this.y + 10);
+            ctx.fillText("sX: " + Math.round(this.screenX) + " | sY: " + Math.round(this.screenY), this.x, this.y + 20);
+            ctx.fillText("wX: " + Math.round(this.worldX) + "  | wY: " + Math.round(this.worldX), this.x, this.y + 30);
+        }
 
-    //if (this.state === this.states.RELOADING) {
-    //    this.animations.runFeet.drawFrame(this.game.clockTick, ctx, this.x + 12, this.y + 17, this.scale);
-    //    this.animations.reloadPistol.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
-    //}
+        //if (this.state === this.states.RELOADING) {
+        //    this.animations.runFeet.drawFrame(this.game.clockTick, ctx, this.x + 12, this.y + 17, this.scale);
+        //    this.animations.reloadPistol.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
+        //}
 
-    //Check for collisions with zombies
-    for (var i = 0; i < this.game.entities.length; i++) {
-        var ent = this.game.entities[i];
-        if (ent.name === "Zombie" && !ent.isDead) {
-            var currentZombie = this.hitbox.getCollisionInfo(ent);
-            if (currentZombie.hit) {
-                if (globals.debug) console.log("Bit by a zombie!");
+        //Check for collisions with zombies
+        for (var i = 0; i < this.game.entities.length; i++) {
+            var ent = this.game.entities[i];
+            if (ent.name === "Zombie" && !ent.isDead) {
+                var currentZombie = this.hitbox.getCollisionInfo(ent);
+                if (currentZombie.hit) {
+                    if (globals.debug) console.log("Bit by a zombie!");
 
-                var knockback = 20;
+                    var knockback = 20;
 
-                if (currentZombie.dirs.top) {
-                    this.y -= knockback;
-                }
-                if (currentZombie.dirs.right) {
-                    this.x += knockback;
-                }
-                if (currentZombie.dirs.down) {
-                    this.y += knockback;
-                }
-                if (currentZombie.dirs.left) {
-                    this.x -= knockback;
-                }
+                    if (currentZombie.dirs.top) {
+                        this.y -= knockback;
+                    }
+                    if (currentZombie.dirs.right) {
+                        this.x += knockback;
+                    }
+                    if (currentZombie.dirs.down) {
+                        this.y += knockback;
+                    }
+                    if (currentZombie.dirs.left) {
+                        this.x -= knockback;
+                    }
 
-                if (!globals.mute) {
-                    if (this.audio.src !== "./sound/pain.wav") this.audio.src = "./sound/pain.wav";
-                    this.audio.play();
-                }
-
-                if (!this.godlike)
-                    this.health -= 5;
-
-                if (this.health <= 0) {
                     if (!globals.mute) {
-                        this.audio.src = "./sound/death.wav";
+                        if (this.audio.src !== "./sound/pain.wav") this.audio.src = "./sound/pain.wav";
                         this.audio.play();
                     }
-                    this.removeFromWorld = true;
+
+                    if (!this.godlike)
+                        this.health -= 5;
+
+                    if (this.health <= 0) {
+                        if (!globals.mute) {
+                            this.audio.src = "./sound/death.wav";
+                            this.audio.play();
+                        }
+                        this.removeFromWorld = true;
+                    }
                 }
             }
         }
+
+
+        if (this.drawLazer) {
+            console.log("lazer time");
+            lineToMouse(ctx, this.x + this.animations.idle.width / 2, this.y + this.animations.idle.height / 2)
+
+
+        }
+
+        if (globals.debug) this.hitbox.draw(ctx);
+
+        Entity.prototype.draw.call(this);
     }
-
-
-    if (this.drawLazer) {
-        console.log("lazer time");
-        lineToMouse(ctx, this.x + this.animations.idle.width / 2, this.y + this.animations.idle.height / 2)
-
-
-
-    }
-
-    if (globals.debug) this.hitbox.draw(ctx);
-
-    Entity.prototype.draw.call(this);
 };
 
-Player.prototype.grabPowerups = function() {
+Player.prototype.grabPowerups = function () {
 
     for (var i = 0; i < this.game.entities.length; i++) {
         var current = this.game.entities[i];
@@ -415,7 +412,7 @@ Player.prototype.grabPowerups = function() {
 
 };
 
-Player.prototype.convertToOnScreen = function() {
+Player.prototype.convertToOnScreen = function () {
     this.isOnScreen = false;
     var convert = worldToScreen(this.x, this.y);
     //this.x = convert.x;
@@ -425,7 +422,7 @@ Player.prototype.convertToOnScreen = function() {
     //this.hitbox.updateXY(this.screenX + (this.animations.idle.frameWidth / 2), this.screenY + (this.animations.idle.frameHeight / 2));
 };
 
-Player.prototype.convertToOffScreen = function() {
+Player.prototype.convertToOffScreen = function () {
     this.isOnScreen = true;
     var convert = screenToWorld(this.x, this.y);
     //this.x = convert.x;
@@ -448,7 +445,10 @@ Player.prototype.isCollidingWith = function (entity) {
     };
 
 
-    return {hit: distance(this.hitbox, entity.hitbox) < this.hitbox.radius + entity.hitbox.radius, dirs: collisions};
+    return {
+        hit: distance(this.hitbox, entity.hitbox) < this.hitbox.radius + entity.hitbox.radius,
+        dirs: collisions
+    };
 };
 /**
  *
@@ -471,7 +471,7 @@ function lineToMouse(ctx, x, y) {
 
     ctx.save();
     ctx.beginPath();
-    ctx.fillStyle="rgba(255, 0, 0, 0.5)";
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
 
     ctx.moveTo(x, y);
     ctx.lineTo(x + lineX, y + lineY);
@@ -479,5 +479,7 @@ function lineToMouse(ctx, x, y) {
     ctx.stroke();
     ctx.restore();
 }
+
+
 
 
