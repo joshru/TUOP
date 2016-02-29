cdffd38c4616a49d27901f50a9b0fe9a150423ab
Jeffrey LeCompte <jeffreylec@gmail.com>
2016-02-28 22:20:22 -0800

    update: added win text with replay.

--- scripts/main.js
+++ scripts/main.js
@@ -152,28 +152,27 @@ Background.prototype.draw = function (ctx) {
     //ctx.fillText("Mute me", 10, 80).ondblclick.apply(document.getElementById("soundFX").muted = true);
 
     /* Screen get's bloodier */
-    if (globals.player.health >= 0) {
-        opacity += 0.3 - (globals.player.health / 100);
-        ctx.fillStyle = "rgba(195, 0, 0, " + opacity + ")";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.font = "30px Courier New";
-        ctx.fillStyle = "white";
-        ctx.fillText("Player Health: " + globals.player.health, 10, 30);
-    }
-    /*Display player health*/
+    opacity += 0.3 - (globals.player.health / 100);
+    ctx.fillStyle = "rgba(195, 0, 0, " + opacity + ")";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "30px Courier New";
+    ctx.fillStyle = "white";
+    ctx.fillText("Player Health: " + globals.player.health, 10, 30);
+
+    /* Death Scene */
     if (globals.player.health <= 0) {
         ctx.fillStyle = "rgba(195, 0, 0, " + 0.5 + ")";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         ctx.fillStyle = "white";
         ctx.font = "50px Courier New";
-        ctx.fillText("YOU DEAD HOMIE rip", 120, canvas.height / 2);
+        ctx.fillText("YOU DEAD HOMIE rip", 120, canvas.height / 2 - 50);
         // line below stops updating the game (we can keep this or lose this).
         this.game.gameStates.GAMEOVER = true;
 
-        /* for splash screen and start */
+        /* for replaying */
         var startOrReplay = {
             x: 320,
-            y: canvas.height / 2 - 50,
+            y: canvas.height / 2,
             w: 150,
             h: 30
         };
@@ -181,21 +180,27 @@ Background.prototype.draw = function (ctx) {
         canvas.addEventListener("mousedown", restartGame, false);
 
         ctx.fillText("REPLAY", startOrReplay.x, startOrReplay.y);
+    }
 
-        function restartGame(event) {
-            console.log("testing");
-            var rect = canvas.getBoundingClientRect();
-            var canvas_x = Math.round(event.clientX - rect.left);
-            var canvas_y = Math.round(event.clientY - rect.top);
+    if (globals.waveNumber === 1) {
+        this.game.gameStates.GAMEOVER = true;
 
-            //console.log("x=" + canvas_x + " y= " + canvas_y + "startext y: " + startText.y);
-            if (canvas_x >= startOrReplay.x && canvas_x <= startOrReplay.x + startOrReplay.w &&
-                canvas_y >= startOrReplay.y - startOrReplay.h && canvas_y <= startOrReplay.y) {
-                startOrReplay = {x: undefined, y: undefined, w: undefined, h: undefined};
-                // reloads page - restart all states and canvas is much more complicated
-                location.reload();
-            }
-        }
+        /* for replaying */
+        var startOrReplay = {
+            x: 320,
+            y: canvas.height / 2,
+            w: 150,
+            h: 30
+        };
+
+        canvas.addEventListener("mousedown", restartGame, false);
+
+        ctx.fillStyle = "rgba(0, 195, 0, " + 0.5 + ")";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "white";
+        ctx.font = "50px Courier New";
+        ctx.fillText("REPLAY", startOrReplay.x, startOrReplay.y);
+        ctx.fillText("YOU WIN!", startOrReplay.x - 25, startOrReplay.y - 50);
     }
 
     /* Display Godlike */
@@ -213,11 +218,22 @@ Background.prototype.draw = function (ctx) {
     ctx.fillText("Wave: " + globals.waveNumber, 200, 60);
     ctx.fillText("Kills: " + globals.killCount, 10, 60);
 
+    function restartGame(event) {
+        console.log("testing");
+        var rect = canvas.getBoundingClientRect();
+        var canvas_x = Math.round(event.clientX - rect.left);
+        var canvas_y = Math.round(event.clientY - rect.top);
+
+        if (canvas_x >= startOrReplay.x && canvas_x <= startOrReplay.x + startOrReplay.w &&
+            canvas_y >= startOrReplay.y - startOrReplay.h && canvas_y <= startOrReplay.y) {
+            startOrReplay = {x: undefined, y: undefined, w: undefined, h: undefined};
+            // reloads page - restart all states and canvas is much more complicated
+            location.reload();
+        }
+    }
+
     Entity.prototype.draw.call(this);
 };
-
-
-
 
 /**
  * Global Key object
